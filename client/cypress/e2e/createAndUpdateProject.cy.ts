43861ba95977fce7264bf1d6c64feb387aedc8b7
Ivan Afanasev <diykarelia@gmail.com>
2023-05-03 17:36:53 +0200

    uncommented

--- client/cypress/e2e/createAndUpdateProject.cy.ts
+++ client/cypress/e2e/createAndUpdateProject.cy.ts
@@ -9,23 +9,23 @@ describe('Create project', () => {
     cy.location('pathname').should('eq', '/home');
   });
 
-  // it('should find the Create button', () => {
-  //   cy.get('button[id="createButton"]');
-  // });
+  it('should find the Create button', () => {
+    cy.get('button[id="createButton"]');
+  });
 
-  // it('should find the project creation form, fill out all the fields, and submit', () => {
-  //   cy.get('button[id="createButton"]').click({ force: true });
-  //   cy.contains('Your new project');
-  //   cy.get('input[id="title"]').type('Cypress Cool Test');
-  //   cy.get('[id="quill"]').type('Description for Cypress Cool Test');
-  //   cy.fixture('unsplash.jpg', null).as('myFixture');
-  //   cy.get('input[type=file]').selectFile('@myFixture');
-  //   cy.get('input[id="tags"]').type('test cypress');
-  //   cy.get('button[id="submit-new-project"]').click();
-  //   cy.wait(5000);
-  // });
+  it('should find the project creation form, fill out all the fields, and submit', () => {
+    cy.get('button[id="createButton"]').click({ force: true });
+    cy.contains('Your new project');
+    cy.get('input[id="title"]').type('Cypress Cool Test');
+    cy.get('[id="quill"]').type('Description for Cypress Cool Test');
+    cy.fixture('unsplash.jpg', null).as('myFixture');
+    cy.get('input[type=file]').selectFile('@myFixture');
+    cy.get('input[id="tags"]').type('test cypress');
+    cy.get('button[id="submit-new-project"]').click();
+    cy.wait(5000);
+  });
 
-  it('should find the project in /home and click "Read more"', () => {
+  it('should update the project via the Update Form', () => {
     cy.get('<h1>Cypress Cool Test</h1>');
     cy.get('button[id="Cypress Cool Test"]').click({ multiple: true });
     cy.get('button[id="updateButton"]').click({ force: true });
